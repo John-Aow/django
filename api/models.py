5395301533ead42284b9354fb130315bee779a9d
John <jirayu.aow@gmail.com>
2023-08-17 17:12:32 +0700

    save

--- api/models.py
+++ api/models.py
@@ -11,3 +11,95 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title 
+    
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    tel = models.CharField(max_length=11)
+    wallet = models.FloatField(default=0)
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    detail = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    qty = models.IntegerField()
+    created_at = models.DateField(default=None, blank=True, null=True)
+    updated_at = models.DateField(default=None, blank=True, null=True)
+    CART = "CA"
+    SHIPMANET = "SH"
+    ORDER = "OR"
+    STATUS = [
+        (CART, "cart"),
+        (ORDER, "order"),
+        (SHIPMANET, "shipment"),
+    ]
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS,
+        default=CART,
+    )
+    def __str__(self):
+        return self.product_id 
+    
+
+class Shipment(models.Model):
+    track_no = models.CharField(max_length=100)
+    address = models.IntegerField()
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    shipment_date = models.IntegerField()
+    recieved_date = models.IntegerField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+    SUCCESS = "SU"
+    PENDING = "PE"
+    SHIPPING = "SH"
+    STATUS = [
+        (SUCCESS, "success"),
+        (SHIPPING, "shipping"),
+        (SUCCESS, "success"),
+    ]
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS,
+        default=PENDING,
+    )
+    def __str__(self):
+        return self.product_id 
+    
+class ShipmentDetail(models.Model):
+    order = models.IntegerField()
+    shipment = models.IntegerField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+    def __str__(self):
+        return self.order_id
+    
+class Admin(models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    tel = models.CharField(max_length=11)
+    password = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Address(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    house_no = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    sub_district = models.CharField(max_length=50)
+    provine = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=11)
+    def __str__(self):
+        return self.name
--- api/schema.py
+++ api/schema.py
@@ -1,13 +1,169 @@
 import graphene
-
+import datetime
 from graphene_django import DjangoObjectType, DjangoListField 
-from .models import Book 
+from .models import Book, User, Order, Product, Admin, Address, Shipment, ShipmentDetail
+from graphene_django.filter import DjangoFilterConnectionField
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        field = "__all__"
+
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
+        field = "__all__"
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        field = "__all__"
+
+class AdminType(DjangoObjectType):
+    class Meta:
+        model = Admin
+        field = "__all__"
+
+class AddressType(DjangoObjectType):
+    class Meta:
+        model = Address
+        field = "__all__"
+
+class ShipmentType(DjangoObjectType):
+    class Meta:
+        model = Shipment
+        field = "__all__"
+
+class ShipmentDetailType(DjangoObjectType):
+    class Meta:
+        model = ShipmentDetail
+        field = "__all__"
 
 
 class BookType(DjangoObjectType): 
     class Meta:
         model = Book
         fields = "__all__"
+
+
+class UserInput(graphene.InputObjectType):
+    id = graphene.ID()
+    email = graphene.String()
+    lastname = graphene.String()
+    name = graphene.String()
+    tel = graphene.String()
+
+
+class ProductInput(graphene.InputObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    price = graphene.Float()
+    detail = graphene.String()
+
+class OrderInput(graphene.InputObjectType):
+    id = graphene.ID()
+    product = graphene.Int()
+    user = graphene.Int()
+    qty = graphene.Int()
+    status = graphene.String()
+    created_at = graphene.DateTime()
+    updated_at = graphene.DateTime()
+
+class AddressInput(graphene.InputObjectType):
+    id = graphene.ID()
+    house_no = graphene.String()
+    district = graphene.String()
+    sub_district = graphene.String()
+    provine = graphene.String()
+    street = graphene.String()
+    postcode = graphene.String()
+
+class CreateUser(graphene.Mutation):
+    class Arguments:
+        user_data = UserInput(required=True)
+    user = graphene.Field(UserType)
+
+    @staticmethod
+    def mutate(root, info, user_data=None):
+        user_instance = User( 
+            name=user_data.name,
+            lastname=user_data.lastname,
+            email=user_data.email,
+            tel=user_data.tel
+        )
+        user_instance.save()
+        return CreateUser(user=user_instance)
+
+class UpdateUser():
+    class Arguments:
+        user_data = UserInput(require=True)
+    user = graphene.Field(UserType)
+    @staticmethod
+    def mutate(root, info, user_data=None):
+
+        user_instance = User.objects.get(pk=user_data.id)
+
+        if user_instance:
+            user_instance.name=user_data.name,
+            user_instance.lastname=user_data.lastname,
+            user_instance.email=user_data.email,
+            user_instance.tel=user_data.tel
+
+            return UpdateUser(user=user_instance)
+        return UpdateUser(user=None)
+    
+class CreateProduct(graphene.Mutation):
+    class Arguments:
+        product_data = ProductInput(required=True)
+    product = graphene.Field(ProductType)
+
+    @staticmethod
+    def mutate(root, info, product_data=None):
+        product_instance = Product( 
+            
+            name=product_data.name,
+            price=product_data.price,
+            detail=product_data.detail,
+        )
+        product_instance.save()
+        return CreateProduct(product=product_instance)
+    
+class CreateOrder(graphene.Mutation):
+    class Arguments:
+        order_data = OrderInput(required=True)
+    order = graphene.Field(OrderType)
+    @staticmethod
+    def mutate(root, info, order_data=None):
+        now = datetime.datetime.now()
+        order_instance = Order.objects.create( 
+            qty=order_data.qty,
+            user=User.objects.get(pk=order_data.user),
+            product=Product.objects.get(pk=order_data.product),
+            created_at=now
+        )
+
+        order_instance.save()
+        return CreateOrder(order=order_instance)
+    
+class CreateAddress(graphene.Mutation):
+    class Arguments:
+        address_data = AddressInput(required=True)
+    address = graphene.Field(OrderType)
+    @staticmethod
+    def mutate(root, info, address_data=None):
+        now = datetime.datetime.now()
+        address_instance = Order.objects.create( 
+            house_no = address_data.house_no,
+            district = address_data.district,
+            sub_district = address_data.sub_district,
+            provine = address_data.provine,
+            street = address_data.street,
+            postcode = address_data.postcode,
+            user=User.objects.get(pk=address_data.user),
+        )
+
+        address_instance.save()
+        return CreateAddress(address=address_instance)
 
 class BookInput(graphene.InputObjectType):
     id = graphene.ID()
@@ -70,7 +226,46 @@ class DeleteBook(graphene.Mutation):
 
 class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
+    
     book = graphene.Field(BookType, book_id=graphene.Int())
+
+    all_user = graphene.List(UserType)
+
+    all_product = graphene.List(ProductType)
+
+    all_order = graphene.List(OrderType)
+
+    all_address = graphene.List(AddressType)
+
+    order = graphene.Field(OrderType, order_id=graphene.Int())
+
+    product = graphene.Field(ProductType, product_id=graphene.Int())
+    
+    user = graphene.Field(UserType, user_id=graphene.Int())
+
+    find_order = graphene.Field(OrderType, user_id=graphene.Int())
+
+
+    def resolve_all_user(self, info, **kwargs):
+        return User.objects.all()
+    
+    def resolve_all_product(self, info, **kwargs):
+        return Product.objects.all()
+    
+    def resolve_all_order(self, info, **kwargs):
+        return Order.objects.all()
+   
+    def resolve_product(self, info, product_id):
+        return Product.objects.get(pk=product_id)
+    
+    def resolve_find_order(self, info, user_id):
+        return Order.objects.filter(user=user_id)
+    
+    def resolve_order(self, info, order_id):
+        return Order.objects.get(pk=order_id)
+
+    def resolve_user(self, info, user_id):
+        return User.objects.get(pk=user_id)
 
     def resolve_all_books(self, info, **kwargs):
         return Book.objects.all()
@@ -82,5 +277,7 @@ class Mutation(graphene.ObjectType):
     create_book = CreateBook.Field()
     update_book = UpdateBook.Field()
     delete_book = DeleteBook.Field()
-
+    create_user = CreateUser.Field()
+    create_product = CreateProduct.Field()
+    create_order = CreateOrder.Field()
 schema = graphene.Schema(query=Query, mutation=Mutation)
